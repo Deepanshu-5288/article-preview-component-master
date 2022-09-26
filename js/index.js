0f86ca174947663803898946419daac874809136
Deeta-max <deepanshugupta5288@gmail.com>
2022-09-26 20:10:02 +0530

    final commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.querySelector(".share-img").addEventListener("click", handleClick);
+function handleClick1(){
+    alert("please add ");
+}
+function handleClick(){
+    document.querySelector(".share-div").classList.toggle("share-div-show");
+    document.querySelector(".right-section-bottom").classList.toggle("right-section-bottom-hide");
+}
